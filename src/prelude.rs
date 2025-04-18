54f0215f18ae3c352af4a328e922746d6f02ba39
dariushghassemieh <dariush@ghassemieh.com>
2025-04-17 23:18:30 -0700

    1

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,1 +1,9 @@
+use bevy::app::App;
 pub use crate::{bodies::*, collisions::*, rays::*};
+
+pub struct ZphyPlugin;
+impl bevy::app::Plugin for ZphyPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins((CollisionPlugin, RigidBodyPlugin));
+    }
+}
